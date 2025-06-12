a67f9f6042b3edaf5357ad7303b893acffd90af8
Androz2091 <androz2091@gmail.com>
2025-06-12 20:45:40 +0200

    fix maxsubarray

--- maxsubarray.go
+++ maxsubarray.go
@@ -2,12 +2,29 @@ package main
 
 import "math"
 
+/*
+On a une entrée $[0, 2, -4, 3, -1, 4, 5, 7, -9]$ et on veut trouver le sous-intervalle avec la plus grande somme $[3, -1, 4, 5, 7]$.
+
+On peut tout bruteforce, tester toutes les combinaisons : $O(n^2)$.
+
+On peut faire mieux en $n log n$ avec du divide et conquer :
+- on peut séparer le problème en deux (c'est facile on coupe au milieu)
+- mais comment combiner les deux, une fois qu'on a les solutions de l'un et de l'autre ? (en reprenant l'exemple plus haut, $[3], [5,7]$).
+	- on connaît la solution à gauche
+	- on connaît la solution à droite
+	- mais on doit vérifier qu'en se chevauchant on a pas une meilleure solution $O(n)$:
+		- pour ça on doit trouver la plus grande somme à gauche qui commence au milieu (ici $3 - 1 = 2$)
+		- trouver la plus grande somme à droite qui commence au milieu (ici $4 + 5 + 7 = 16$)
+		- les sommer ($2 + 16 = 18$)
+	- comparer ces trois solutions prendre la meilleure
+*/
+
 func maxsubarray(array []int, begin int, end int) int {
 
 	if begin < end {
 		middle := int(math.Floor(float64(begin+end) / 2))
 
-		maxCrossingLeft := 0
+		maxCrossingLeft := int(math.MinInt64)
 		currCrossingLeft := 0
 		for i := middle; i >= begin; i-- {
 			currCrossingLeft += array[i]
@@ -16,7 +33,7 @@ func maxsubarray(array []int, begin int, end int) int {
 			}
 		}
 
-		maxCrossingRight := 0
+		maxCrossingRight := int(math.MinInt64)
 		currCrossingRight := 0
 		for i := middle + 1; i <= end; i++ {
 			currCrossingRight += array[i]
