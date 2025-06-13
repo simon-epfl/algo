c91b05bf1c665a30c94cd92324b8ced1de109c1f
Androz2091 <androz2091@gmail.com>
2025-06-13 11:52:08 +0200

    add more custom heaps

--- custom_heaps.go
+++ custom_heaps.go
@@ -61,10 +61,54 @@ func (h *CustomHeap) maxHeapify(i int) {
 
 }
 
-func (h CustomHeap) buildMaxHeap() {
+func (h *CustomHeap) buildMaxHeap() {
 	// on veut appeler max heapify à partir du bas sur chaque noeud
 
 	for i := (h.Len() - 2) / 2; i >= 0; i-- {
 		h.maxHeapify(i)
 	}
+}
+
+func (h *CustomHeap) heapMaximum() CustomHeapItem {
+	return (*h)[0]
+}
+
+func (h *CustomHeap) heapExtractMax() CustomHeapItem {
+	heap := *h
+
+	if heap.Len() == 0 {
+		panic("heap underflow")
+	}
+
+	max := heap.heapMaximum()
+	heap[0], heap[heap.Len()-1] = heap[heap.Len()-1], heap[0]
+	heap = heap[:heap.Len()-1]
+
+	heap.maxHeapify(0)
+
+	return max
+}
+
+func (h *CustomHeap) heapIncreaseKey(idx int, value int) {
+
+	heap := *h
+
+	heap[idx].Value = value
+
+	// tant que le parent est plus petit, on swap pour faire remonter la nouvelle clef
+	for idx > 0 && heap[heap.Parent(idx)].Value < heap[idx].Value {
+		heap[idx], heap[heap.Parent(idx)] = heap[heap.Parent(idx)], heap[idx]
+		idx = heap.Parent(idx)
+	}
+
+}
+
+func (h *CustomHeap) maxHeapInsert(item CustomHeapItem) {
+	heap := *h
+
+	heap = append(heap, item)
+
+	// à heap.Len() -1 on va avoir l'élément qu'on vient juste d'ajouter
+	// on trigger un nouveau calcul de sa position
+	heap.heapIncreaseKey(heap.Len()-1, item.Value)
 }
