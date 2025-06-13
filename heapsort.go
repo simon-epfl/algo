17e8855e71b98662393523999490307f0b607034
Androz2091 <androz2091@gmail.com>
2025-06-13 11:36:26 +0200

    heapsort

--- heapsort.go
+++ heapsort.go
@@ -1,35 +1,26 @@
 package main
 
 import (
-	"algo/shared"
-	"container/heap"
 	"fmt"
 )
 
-// c'est pas exactement l'implémentation de heapsort classique,
-// mais ça utilise une priority queue pour trier les entiers
-// en O(n log n) temps
-
-func HeapSortInts(input []int) []int {
-	// On créé une priority queue vide
-	pq := make(shared.PriorityQueue, 0, len(input))
-	heap.Init(&pq)
-
-	// On envoie toutes nos valeurs dans la queue
-	for _, v := range input {
-		item := &shared.PriorityItem{
-			Value:    v,
-			Priority: v, // la prio de l'item c'est sa valeur elle-même
-		}
-		heap.Push(&pq, item)
+func customHeapSortInts(input []int) []int {
+	var heap CustomHeap
+	for i := range input {
+		heap = append(heap, CustomHeapItem{Value: i})
 	}
+	heap.buildMaxHeap()
 
-	// tout est déjà trié ici !
+	fmt.Println(heap)
 
-	sorted := make([]int, 0, len(input))
-	for pq.Len() > 0 {
-		item := heap.Pop(&pq).(*shared.PriorityItem)
-		sorted = append(sorted, item.Value.(int))
+	var sorted []int
+
+	for heap.Len() > 0 {
+		sorted = append(sorted, heap[0].Value)
+		heap[0] = heap[heap.Len()-1] // on prend le dernier élément et on le met à la place du root
+		heap = heap[:heap.Len()-1]   // on enlève le dernier élément
+
+		heap.maxHeapify(0)
 	}
 
 	return sorted
@@ -37,7 +28,7 @@ func HeapSortInts(input []int) []int {
 
 func runheapsort() {
 	unsorted := []int{5, 1, 7, 3, 2, 9, 4}
-	sorted := HeapSortInts(unsorted)
+	sorted := customHeapSortInts(unsorted)
 	fmt.Println("Non trié:", unsorted)
 	fmt.Println("Trié:  ", sorted)
 }
